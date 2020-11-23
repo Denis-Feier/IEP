0dea7931a2780e80cc16d7332ed975a4b19aff28
DenisuDevForFood <denisfeier98@yahoo.com>
2020-11-24 00:25:01 +0200

    delete useless files

--- Lab8/Test/.text.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/denisu/IEP/Lab8/Test/text.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/denisu/IEP/Lab8/Test/text.ko /home/denisu/IEP/Lab8/Test/text.o /home/denisu/IEP/Lab8/Test/text.mod.o ;  true
